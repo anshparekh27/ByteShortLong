bfafb596270d4d8dc4fdb73368dd8eb02bbc41bb
anshp <anshparekh03@gmail.com>
2023-06-06 22:24:35 +0530

    modified

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+            byte max_value=Byte.MAX_VALUE;
+            byte min_value=Byte.MIN_VALUE;
+
+            System.out.println("Byte max value is " + max_value);
+            System.out.println("Byte min value is " + min_value);
+
+            short maxVal =Short.MAX_VALUE;
+            short minVal=Short.MIN_VALUE;
+
+            System.out.println("Short max value is " + maxVal);
+            System.out.println("Short min value is " + minVal);
+
+            long maxValue1=Long.MAX_VALUE;
+            long minValue1=Long.MIN_VALUE;
+
+            System.out.println("Long max value is " + maxValue1);
+            System.out.println("Long min value is " + minValue1);
+
+        }
+    }
